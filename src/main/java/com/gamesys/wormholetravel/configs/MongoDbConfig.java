0a5a58dd6eddbb9fdd59eab2e5becf9cce584668
Ricardo S Araujo <ricardo.araujo@kskmail.com.br>
2018-06-27 15:05:14 -0300

    rollback DB config

--- src/main/java/com/gamesys/wormholetravel/configs/MongoDbConfig.java
+++ src/main/java/com/gamesys/wormholetravel/configs/MongoDbConfig.java
@@ -1,18 +1,13 @@
 package com.gamesys.wormholetravel.configs;
 
-import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@Configuration
-@EnableMongoRepositories("com.gamesys.wormholetravel.repositories")
+//@Configuration
+//@EnableMongoRepositories("com.gamesys.wormholetravel.repositories")
 public class MongoDbConfig {
 
     @Value("${db.url}")
